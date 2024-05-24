b2dab656bb2d75d5227831862d889c178f8c6b04
Miraamal <esmira.abdullayeva0784@gmail.com>
2024-05-24 18:57:34 +0200

    method has been created with date and time

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,34 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+      writeDateAndTime("Current date.txt");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+    }
+    public static void writeDateAndTime(String fileName){
+        try {
+            Files.writeString(Path.of(fileName), LocalDateTime.now().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
+
+/*
+ В ранее склонированном репозитории одногруппника в новой ветке создайте новый метод в классе Main,
+  который (выберите один из списка):
+выводит в консоль переменные окружения
+создаёт файл, в который записывает текущую дату и время
+скачивает картинку из интернета и сохраняет её на компьютере
+2 Сделайте коммит изменений и запуште их.
+3 Создайте пул-реквест на изменения.
+4 Проведите код-ревью пул-реквеста, который выставили в Вашем репозитории.
+ */
