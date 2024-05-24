a6716fe11a405c7df3713caa199992a686560843
Miraamal <esmira.abdullayeva0784@gmail.com>
2024-05-24 19:22:37 +0200

    fixed after review,removed comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
       writeDateAndTime("Current date.txt");
@@ -22,13 +20,3 @@ public class Main {
     }
 }
 
-/*
- В ранее склонированном репозитории одногруппника в новой ветке создайте новый метод в классе Main,
-  который (выберите один из списка):
-выводит в консоль переменные окружения
-создаёт файл, в который записывает текущую дату и время
-скачивает картинку из интернета и сохраняет её на компьютере
-2 Сделайте коммит изменений и запуште их.
-3 Создайте пул-реквест на изменения.
-4 Проведите код-ревью пул-реквеста, который выставили в Вашем репозитории.
- */
